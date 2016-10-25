124391d05aeab9ac16ce216c4b335ba098d5a4da
ElJerry <kanegerardo_@hotmail.com>
2016-10-25 11:00:36 -0500

    Update
    Se trabajo en ClienteDAO y ProductoDAO

--- src/java/paq/ClienteDAO.java
+++ src/java/paq/ClienteDAO.java
@@ -5,10 +5,47 @@
  */
 package paq;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Gerardo
  */
 public class ClienteDAO {
+    
+    public static Connection getConnection(){
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/examen2p","root","");
+        }
+        catch(ClassNotFoundException | SQLException e){
+            System.out.println(e);
+        }
+        
+        return con;
+    }
+    
+    public static List<Cliente> getAllRecords(){
+        
+        List<Cliente> list = new ArrayList<>();
+        
+        try{
+            Connection con = getConnection();
+            PreparedStatement ps = con.prepareStatement("Select * from cliente");
+            
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Cliente u = new Cliente();
+                u.setId(rs.getInt("id_c"));
+                u.setSaldo(rs.getInt("saldo"));
+                u.setNombre(rs.getString("nombre"));
+                list.add(u);
+            }
+        }catch(SQLException e){ System.out.println(e);}
+        return list;
+    }
     
 }
--- src/java/paq/ProductoDAO.java
+++ src/java/paq/ProductoDAO.java
@@ -5,10 +5,47 @@
  */
 package paq;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Gerardo
  */
-public class ProductoDAO {
+public class ProductoDAO{
+
+    public static Connection getConnection(){
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/examen2p","root","");
+        }
+        catch(ClassNotFoundException | SQLException e){
+            System.out.println(e);
+        }
+
+        return con;
+    }
+    
+    public static List<Producto> getAllRecords(){
+        
+        List<Producto> list = new ArrayList<>();
+        
+        try{
+            Connection con = getConnection();
+            PreparedStatement ps = con.prepareStatement("Select * from producto");
+            
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Producto p = new Producto();
+                p.setId(rs.getInt("id_p"));
+                p.setPrecio(rs.getInt("precio"));
+                p.setNombre(rs.getString("nombre"));
+                list.add(p);
+            }
+        }catch(SQLException e){ System.out.println(e);}
+        return list;
+    }
     
 }
